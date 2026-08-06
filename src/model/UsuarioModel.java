8944d76a9925405ae7d7cbca3d70ee01d0df4c49
FATALvtr <vitordias1502@gmail.com>
2026-08-05 21:55:48 -0300

    first commit

--- src/model/UsuarioModel.java
+++ src/model/UsuarioModel.java
@@ -8,8 +8,55 @@ public class UsuarioModel {
     private String cpf;
     private String email;
     private String telefone;
-    private int idade;
     private Date nascimento;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
     
     
     
